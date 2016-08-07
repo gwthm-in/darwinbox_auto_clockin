b1fd44ac6564a3ce31186cb27e6f024330056c98
Gowtham Sai <gowtham.s@voonik.com>
2016-08-08 05:11:12 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# darwinbox_auto_clockin
+## Python script to automatically clock in darwinbox. Uses Mac OSX launchctl to run every 10  minutes. One it clocks in, it will send message to your mobile too. 
+
+###### Author: Gowtham Sai
+###### Website: https://gowtham-sai.com
+###### Aritcle: blog.gowtham-sai.com (Will be updated soon)
+###### Date: 7th Aug, 2016.
+###### Purpose: I frustated with darwin box. I don't remember to clock in daily. 
+###### What this script do?
+######		-- This scritps run automatically checks every minute whether you clocked in or not.
+######			If not cloked in, it will clock in for you. 
+###### So do I need to run the script?
+######		-- No/Yes. (You can set cronjob if you want in your mac/linux) / (Task Schedular in Windows Box)
+######
+###### Sigstamp: 7h3 !n5|d3r
+#
+#
+#
+# This needs
+This script needs python, Python Mechanize Library, Cookies, Your Username (Email) and Password (DarwinBox), Your Mobile and Password (Way2SMS), OSX Launchctl to run the script every 10 minutes.
+
+` Most of the times, this throws error, importing module mecahnize. So, to eliminate it: `
+  
+    sudo easy_install mechanize
+
+# How to configure:
+Open [darwin.py](darwin.py) file and Change Your Email id, Phone Number, Password
+
+    47.    email = 'YOUR EMAIL'
+    48.    password = 'YOUR PASSWORD HERE'
+    49.    mobile = "MOBILE NUMBER HERE"
+        
+Open [WAY2SMS.py](WAY2SMS.py) file, 
+    
+    23.    username = 'MOBILE NUMBER HERE'
+    24.    password = 'Way2SMS PASSWORD HERE'
+##### save darwin.py and way2sms.py files. 
+#
+#
+#
+#
+# Keep this directory under work in Documents.
+#
+>  Absolutepath: `/Users/{YOUR USERNAME}/Documents/work/darwin/ `
+#
+
+##### If you want to keep this somewhere, please chagne line 19 in com.darwinbox.Helper.plist #
+#
+
+        <string>/Users/GowthamSai/Documents/work/darwin/darwin.py</string>
+replace the above line with your path to darwin.py
+
+#
+#
+# How to use:
+            
+* Copy com.darwinbox.Helper.plist to /Library/LaunchDaemons/ using the command sudo cp ./com.darwinbox.Helper.plist /Library/LaunchDaemons/
+
+` Now you need to launch the job. `
+        
+        2. launchctl load com.darwinbox.Helper.plist    
+    
+` Now you need to start the job. `
+
+        3. launchctl start com.darwinbox.Helper.plist
+
+# All done. Now, you don't need to worry about darwinbox clock in. Forget it. 
+##
+# Proof:
+` Sample Log file from my mac: `
+
+    Mon Aug  8 03:41:49 2016 [INFO] - Opening Darwinbox.
+    Mon Aug  8 03:41:50 2016 [INFO] - Submitting Form.
+    Mon Aug  8 03:41:59 2016 [INFO] - Login Sucessful.
+    Mon Aug  8 03:42:01 2016 [INFO] - User ID 576fc240f0aac.
+    Mon Aug  8 03:42:01 2016 [INFO] - User Clocked Out.
+    Mon Aug  8 03:42:01 2016 [INFO] - Attempting to CLOCK IN.
+    Mon Aug  8 03:44:09 2016 [INFO] - Opening Darwinbox.
+    Mon Aug  8 03:44:10 2016 [INFO] - Submitting Form.
+    Mon Aug  8 03:44:18 2016 [INFO] - Login Sucessful.
+    Mon Aug  8 03:44:20 2016 [INFO] - User ID 576fc240f0aac.
+    Mon Aug  8 03:44:20 2016 [INFO] - User Clocked Out.
+    Mon Aug  8 03:44:20 2016 [INFO] - Attempting to CLOCK IN.
+    Mon Aug  8 03:44:20 2016 [INFO] - CLOCK IN Failed. Reason: Invalid IP<br/>Mon Aug  8    03:54:22 2016 [INFO] - Opening Darwinbox.
+    Mon Aug  8 03:54:23 2016 [INFO] - Submitting Form.
+    Mon Aug  8 03:54:31 2016 [INFO] - Login Sucessful.
+    Mon Aug  8 03:54:33 2016 [INFO] - User ID 576fc240f0aac.
+    Mon Aug  8 03:54:33 2016 [INFO] - User Clocked Out.
+    Mon Aug  8 03:54:33 2016 [INFO] - Attempting to CLOCK IN.
+    Mon Aug  8 03:54:33 2016 [INFO] - CLOCK IN Failed. Reason: Invalid IP<br/>.
+    Mon Aug  8 04:04:34 2016 [INFO] - Opening Darwinbox.
+    Mon Aug  8 04:04:35 2016 [INFO] - Submitting Form.
+    Mon Aug  8 04:04:43 2016 [INFO] - Login Sucessful.
+    Mon Aug  8 04:04:44 2016 [INFO] - User ID 576fc240f0aac.
+    Mon Aug  8 04:04:44 2016 [INFO] - User Clocked Out.
+    Mon Aug  8 04:04:44 2016 [INFO] - Attempting to CLOCK IN.
+    Mon Aug  8 04:04:45 2016 [INFO] - CLOCK IN Failed. Reason: Invalid IP<br/>.
+
+
+
+
+
+# Debugging:
+How to check logs:
+
+* You can chceck logs from /var/log/darwinbox.log
+* You can check error form  /var/log/darwinbox.error.log
+
+
---
+++ darwin.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python2.7
+#
+# Author: Gowtham Sai
+# Website: https://gowtham-sai.com
+# Aritcle: blog.gowtham-sai.com (Will be updated soon)
+# Date: 7th Aug, 2016.
+# Purpose: I frustated with darwin box. I don't remember to clock in daily. 
+# What this script do?
+#		-- This scritps run automatically checks every minute whether you clocked in or not.
+#				If not cloked in, it will clock in for you. 
+# So do I need to run the script?
+#		-- No/Yes. (You can set cronjob if you want in your mac/linux) / (Task Schedular in Windows Box)
+#
+# Sigstamp: 7h3 !n5|d3r
+		
+import mechanize
+import cookielib
+import Way2SMS
+import json
+import os
+import time
+import sys
+
+# Browser Setup
+br = mechanize.Browser()
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+br.set_handle_equiv(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+
+# Logger Initialization
+filedir = os.path.dirname(__file__)
+filepath = os.path.join(filedir,'log.txt')
+logger = open(filepath,'a')
+
+def ok(msg,purpose):
+        logger.write(time.ctime()+': %s - %s.\n'%(purpose,msg))
+        print time.ctime()+": %s - %s."%(purpose,msg)
+
+
+# Darwinbox credentials
+email = 'YOUR EMAIL HERE'
+password = 'YOUR PASSWORD HERE'
+mobile = "MOBILE NUMBER HERE"
+value = email.replace('@', '.')
+
+loginurl = 'https://voonik.darwinbox.in/user/login'
+clockurl = 'https://voonik.darwinbox.in/attendance/attendance/log?user='
+
+sys.stdout.write(time.ctime()+" [INFO] - Opening Darwinbox.\n")
+try:
+	login = br.open(loginurl)
+except Exception as e:
+	sys.stdout.write(time.ctime()+" [FATAL] - Opening Darwinbox Failed. %s\n"%e.message)
+	sys.stderr.write(time.ctime()+" [FATAL] - Opening Darwinbox Failed. %s\n"%e.message)
+
+login.set_data(login.get_data().replace('<form  role="form">', ''))
+br.set_response(login)
+br.select_form(nr=0)
+br.form['UserLogin[username]'] = email
+br.form['UserLogin[password]'] = password
+
+sys.stdout.write(time.ctime()+" [INFO] - Submitting Form.\n")
+
+try:
+	br.submit()
+	if br.geturl() != loginurl:
+		sys.stdout.write(time.ctime()+" [INFO] - Login Sucessful.\n")
+		page = br.response().read()
+		firstmatch = page.find(value)
+		startpos = page[firstmatch::-1].find('{')
+		userdata = json.loads(page[firstmatch-startpos:firstmatch+page[firstmatch:].find('}')+1].replace("'", '"'))
+		sys.stdout.write(time.ctime()+" [INFO] - User ID %s.\n"%userdata['id'])
+		for link in br.links():
+			if 'clock in' in link.text.lower():
+				sys.stdout.write(time.ctime()+" [INFO] - User Clocked Out.\n")
+				clockurl = clockurl+str(userdata['id'])
+				xhr = mechanize.Request(clockurl)
+				xhr.add_header('X-Requested-With', 'XMLHttpRequest')
+				xhr.add_header('Accept', 'application/json, text/javascript, */*')
+				sys.stdout.write(time.ctime()+" [INFO] - Attempting to CLOCK IN.\n")
+				try:
+					data = json.loads(br.open_novisit(xhr).readlines()[-1])
+					if not data.has_key('error'):
+						sys.stdout.write(time.ctime()+" [INFO] - User Sucessfully Clocked In.\n")
+						ok("Sucessfully Clocked In. %s" % resp.code,"Info")
+						log = Way2SMS.login()
+						if log != False:	
+							sys.stdout.write(time.ctime()+" [INFO] - Sending Notification to mobile - %s\n"%mobile)		
+							try:
+								Way2SMS.send(mobile, time.ctime()+": %s - %s."%("Info","Sucessfully Clocked In. %s" % resp.code,))
+							except Exception as e:
+								sys.stdout.write(time.ctime()+" [FATAL] - Moblie Notification Failed. %s\n"%e.message)
+								sys.stderr.write(time.ctime()+" [FATAL] - Moblie Notification Failed. %s\n"%e.message)
+							break
+					else:
+						sys.stdout.write(time.ctime()+" [INFO] - CLOCK IN Failed. Reason: %s.\n"%data['error'])
+				except Exception as e:
+					sys.stdout.write(time.ctime()+" [FATAL] - CLOCK IN Failed. %s\n"%e.message)
+					sys.stderr.write(time.ctime()+" [FATAL] - CLOCK IN Failed. %s\n"%e.message)
+
+except Exception as e:
+	sys.stdout.write(time.ctime()+" [FATAL] - Submitting Form Failed. %s\n"%e.message)
+	sys.stderr.write(time.ctime()+" [FATAL] - Submitting Form Failed. %s\n"%e.message)
+
+
+	
